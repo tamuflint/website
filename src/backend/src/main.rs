c4659880ff7bc545b1ed1efffd9ce841c63b1812
dreuxtl <111068676+dreuxtl@users.noreply.github.com>
2025-06-27 12:27:42 -0500

    Basic file serving functionality for frontend files and core site.

--- src/backend/src/main.rs
+++ src/backend/src/main.rs
@@ -1,11 +1,23 @@
 #[macro_use] extern crate rocket;
+use rocket::fs::{FileServer, relative};
+use rocket::response::Redirect;
 
+// Redirects root to /public/ to access fileserver which includes actual public site
 #[get("/")]
-fn index() -> & 'static str {
-    "Hello, world!"
+fn home() -> Redirect {
+    Redirect::to("/public/index.html")
+}
+
+// To prevent path resolution issues from /public
+// css and js for index will not load at /public but will at /public/
+#[get("/public")]
+fn redirect_public() -> Redirect {
+    Redirect::to("/public/index.html")
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![home, redirect_public])
+        .mount("/public", FileServer::from(relative!("../frontend")))
 }
