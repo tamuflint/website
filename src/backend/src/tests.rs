8382e59842d23de34561c9da942e9850bad0df98
dreuxtl <111068676+dreuxtl@users.noreply.github.com>
2025-07-07 18:40:18 -0500

    Test cases for basic webserver functionality

--- src/backend/src/tests.rs
+++ src/backend/src/tests.rs
@@ -6,6 +6,13 @@ use rocket::http::{Status, ContentType};
 fn root() {
     let client = Client::tracked(rocket()).expect("valid rocket instance");
     let mut response = client.get(uri!(super::home)).dispatch();
+    assert_eq!(response.status(), Status::SeeOther);
+}
+
+#[test]
+fn assets() {
+    let client = Client::tracked(rocket()).expect("valid rocket instance");
+    let mut response = client.get(uri!("/assets/logo.png")).dispatch();
     assert_eq!(response.status(), Status::Ok);
-    assert_eq!(response.content_type(), ContentType::HTML);
+    assert_eq!(response.content_type(), Some(ContentType::PNG));
 }
